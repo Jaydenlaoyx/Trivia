edb20c8f7396d78dd4f4df7d142e7b6db9143af2
Jaydenlaoyx <ylao0002@student.monash.edu>
2024-09-11 17:09:01 +1000

    Added Game title. Minor UI change in colors

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,19 @@ const Home = () => {
 
     return (
         <>
+            <div className="gameTitle">
+                <span className="titleLetter letterType1">T</span>
+                <span className="titleLetter">r</span>
+                <span className="titleLetter">i</span>
+                <span className="titleLetter">v</span>
+                <span className="titleLetter">i</span>
+                <span className="titleLetter">a</span>
+                <span className="titleLetter letterType1"> M</span>
+                <span className="titleLetter">a</span>
+                <span className="titleLetter">n</span>
+                <span className="titleLetter">i</span>
+                <span className="titleLetter">a</span>
+            </div>
             <button className="startGameButton" onClick={handleStartGame}>Start</button>
         </>
     );
